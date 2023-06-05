7c99768c72cdafc7b16d8c626202278e843ebc64
kimteasick <notseph8556@gmail.com>
2023-06-05 16:30:07 +0900

    added 230605

--- recommends/priceFilter.py
+++ recommends/priceFilter.py
@@ -4,12 +4,9 @@ import pandas as pd
 def priceRecommend(nowstamp, coinList, dfList, lowPrice, highPrice):
     print(type(lowPrice), type(highPrice))
     priceL = []
-<<<<<<< HEAD
     print(len(coinList), len(dfList), lowPrice, highPrice)
-=======
     lowPrice = int(lowPrice)
 
->>>>>>> 3abce928ca0a7e06f53fbfe8d86084ca66015f8a
     time = nowstamp - (5 * 60)
     df = pd.DataFrame(dfList)
     df2 = df.loc[df['S_time'] > time]
@@ -17,15 +14,10 @@ def priceRecommend(nowstamp, coinList, dfList, lowPrice, highPrice):
     for coin in coinList:
         print(coin)
         df3 = df2.loc[df['coin_name'] == coin]
-<<<<<<< HEAD
 
         if len(df3) != 0 and df3.iloc[-1]['Close'] > float(lowPrice) and df3.iloc[-1]['Close'] < float(highPrice):
-=======
-        if len(df3) == 0:
-            continue
-        if df3.iloc[-1]['Close'] > float(lowPrice) and df3.iloc[-1]['Close'] < float(highPrice):
->>>>>>> 3abce928ca0a7e06f53fbfe8d86084ca66015f8a
-            priceL.append(coin)
+            if len(df3) == 0:
+                continue
 
     print(len(priceL), 'price List =================================================================')
 
