6eb9dee6b4ea513615c25f79dd712012bda9b386
pkwoo9348 <qkwoo9348gmail.com>
2023-07-11 17:04:12 +0900

    trailingstop 추가

---
+++ trailingStop/models.py
@@ -0,0 +1,71 @@
+from sqlalchemy import Boolean, Column, Integer, String, Float
+from database import Base
+
+class autoTradingStatus(Base):
+    __tablename__ = 'nc_b_now_auto_status_t'
+
+    idx = Column(Integer, primary_key=True)
+
+    status = Column(Integer)
+    start_date = Column(String(100))
+
+class tradingOption(Base):
+    __tablename__ = "nc_b_trading_option_t"
+
+    name = Column(String(100), primary_key=True)
+    insert_time = Column(String(100))
+    update_time = Column(String(100))
+    used = Column(Integer)
+
+class tradingAccountOtion(Base):
+    __tablename__ = "nc_c_account_option_t"
+
+    name = Column(String(100), primary_key=True)
+
+    price_count = Column(Integer)
+    loss_cut_under_percent = Column(Integer)
+    loss = Column(Integer)
+    loss_cut_under_call_price_sell_all = Column(Integer)
+    loss_cut_under_coin_specific_percent = Column(Integer)
+    loss_cut_under_call_price_specific_coin = Column(Integer)
+    loss_cut_over_percent = Column(Integer)
+    gain = Column(Integer)
+    loss_cut_over_call_price_sell_all = Column(Integer)
+    loss_cut_over_coin_specific_percent = Column(Integer)
+    loss_cut_over_call_price_specific_coin = Column(Integer)
+    buy_cancle_time = Column(Integer)
+    sell_cancle_time = Column(Integer)
+    loss = Column(Integer)
+    gain = Column(Integer)
+
+class orderCoin(Base):
+    __tablename__ = 'nc_r_order_coin_t'
+
+    coin = Column(String(100), primary_key=True)
+
+    status = Column(Integer)
+    transaction_time = Column(String(100))
+    conclusion_time = Column(String(100))
+    order_id = Column(String(100))
+    cancel_time = Column(String(100))
+    sell_reason = Column(String(100))
+
+class possessionCoin(Base):
+    __tablename__ = 'nc_r_possession_coin_t'
+    coin = Column(String(100),primary_key=True)
+
+    unit = Column(String(100))
+    price = Column(String(100))
+    total = Column(String(100))
+    fee = Column(String(100))
+    status = Column(Integer)
+    transaction_time = Column(String(100))
+    conclusion_time = Column(String(100))
+    order_id = Column(String(100))
+    cancel_time = Column(String(100))
+    macd_chart = Column(String(100))
+    disparity_chart = Column(String(100))
+    optionName = Column(String(100))
+    trailingstop_flag = Column(Integer)
+    max = Column(String(100))
+
---
+++ trailingStop/trailingStop.py
@@ -0,0 +1,228 @@
+import websocket
+import json
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+import datetime
+import pandas as pd
+from pybithumb import Bithumb
+import askingPrice
+import datetime
+
+try:
+    db = SessionLocal()
+    db: Session
+finally:
+    db.close()
+
+# 테스트 계정
+# secretKey = "ee7741a2e52957613c020ded3c91751c"
+# connenctKey = "ef3d9e8fb9b15ca740150fed18cdaaae"
+
+# 본계정
+secretKey = "07c1879d34d18036405f1c4ae20d3023"
+connenctKey = "9ae8ae53e7e0939722284added991d55"
+
+bithumb = Bithumb(connenctKey, secretKey)
+
+sensingPercent = 0
+trailingPercent = 0
+
+askOption = 0
+
+coinNames = []
+possessionCoin = {}
+accountOption = 0
+
+minute = str(datetime.datetime.now().strftime('%M'))
+
+
+def on_open(ws):
+    print('WebSocket connection opened')
+    # Send a subscribe message to the WebSocket
+    subscribe_data = {
+        "type": "transaction",
+        "symbols": coinNames
+    }
+    ws.send(json.dumps(subscribe_data))
+
+
+def on_message(ws, message):
+    try:
+        global possessionCoin
+        global sensingPercent
+        global trailingPercent
+        global askOption
+        global minute
+        global accountOption
+
+        data = eval(message)
+        symbol = data['content']['list'][0]['symbol']
+        price = float(data['content']['list'][0]['contPrice'])
+
+        print(data)
+
+        # 트레일링 스탑 시작
+        if possessionCoin[symbol]['sensingPrice'] < price and possessionCoin[symbol]['trailingStop'] == 0:
+            possessionCoin[symbol]['trailingStop'] = 1
+
+        # 고가 비교
+        if possessionCoin[symbol]['max'] < price:
+            possessionCoin[symbol]['max'] = price
+            possessionCoin[symbol]['trailingPrice'] = round(
+                (price - ((trailingPercent / 100) * price)), 4)
+
+        if possessionCoin[symbol]['trailingStop'] == 1:
+            if possessionCoin[symbol]['trailingPrice'] > price:
+                try:
+                    status = db.query(models.possessionCoin).filter(
+                        models.possessionCoin.coin == symbol[:-4]).first()
+                    if status.status == 3 or status.status == 5:
+                        print(price)
+                        pass
+
+                    else:
+                        print(symbol, possessionCoin[symbol], price, '매도')
+                        askP = price + \
+                            (askOption * askingPrice.askingPrice(price))
+
+                        print(symbol[:-4], float(askP), possessionCoin[symbol]
+                              ['unit'], "KRW", askingPrice.askingPrice(price))
+
+                        # 매도 주문
+                        orderids = bithumb.sell_limit_order(
+                            symbol[:-4], round(float(askP), 4), possessionCoin[symbol]['unit'], "KRW")
+                        print(orderids)
+
+                        # 주문 테이블 저장
+                        order_coin = models.orderCoin()
+                        order_coin.coin = symbol[:-4]
+                        order_coin.transaction_time = datetime.datetime.now()
+                        order_coin.order_id = orderids[2]
+                        order_coin.cancel_time = str(
+                            datetime.datetime.now() + datetime.timedelta(seconds=accountOption))
+                        order_coin.sell_reason = 'trailing stop'
+
+                        if status.status == 4:
+                            status.status = 5
+                            order_coin.status = 5
+
+                        else:
+                            status.status = 3
+                            order_coin.status = 3
+
+                        db.add(order_coin)
+                        print(order_coin.cancel_time)
+
+                        try:
+                            db.commit()
+                        except:
+                            db.rollback()
+
+                        with open("./sellLog", "a") as file:
+                            file.write(
+                                f'{datetime.datetime.now()}------------------------------------------------------------------\n')
+                            file.write(
+                                f"{'coin':symbol[:-4]}, reason: trailing stop" + '\n')
+                            file.close()
+
+                except Exception as e:
+                    print(e)
+
+        print(symbol, possessionCoin[symbol], price)
+
+        # 보유 코인 확인
+        nowMin = str(datetime.datetime.now().strftime('%M'))
+        if minute != nowMin:
+            minute = nowMin
+            possession = db.query(models.possessionCoin).all()
+
+            print(minute)
+            for coin in possession:
+                try:
+                    coin.trailingstop_flag = possessionCoin[f'{coin.coin}_KRW']['trailingStop']
+                    coin.max = possessionCoin[f'{coin.coin}_KRW']['max']
+                except Exception as e:
+                    print(e)
+
+            try:
+                db.commit()
+            except:
+                db.rollback()
+
+            ws.close()
+            start()
+
+    except Exception as e:
+        print(e)
+
+
+def on_close(ws):
+    print('WebSocket connection closed')
+
+
+def on_error(ws, error):
+    print('Error:', error)
+
+
+def start():
+    global coinNames
+    global possessionCoin
+    global coinList
+    global sensingPercent
+    global trailingPercent
+    global askOption
+    global accountOption
+
+    coinList = db.query(models.possessionCoin).all()
+    useTradingOption = db.query(models.tradingOption).filter(
+        models.tradingOption.used == 1).first()
+    accountOp = db.query(models.tradingAccountOtion).filter(
+        models.tradingAccountOtion.name == useTradingOption.name).first()
+    # trailingOption =
+
+    autoStatus = db.query(models.autoTradingStatus).filter(
+        models.autoTradingStatus.status == 1).first()
+
+    accountOption = accountOp.sell_cancle_time
+
+    if autoStatus == None:
+        print('exit')
+        print('자동 매매 정지')
+        exit()
+
+    sensingPercent = 3
+    trailingPercent = 3
+
+    askOption = 2
+
+    coinNames = []
+    possessionCoin = {}
+
+    for coin in coinList:
+        if coin.status == 1:
+            continue
+
+        coinNames.append(f'{coin.coin}_KRW')
+        trailingCoin = {'buyPrice': float(coin.price), 'sensingPrice': float(coin.price) + (float(coin.price) * (sensingPercent / 100)), 'trailingStop': coin.trailingstop_flag,
+                        'max': float(coin.max), 'trailingPrice': float(coin.max) - (float(coin.max) * (trailingPercent / 100)), 'unit': float(coin.unit)}
+
+        possessionCoin[f'{coin.coin}_KRW'] = trailingCoin
+
+    if len(coinList) == 0:
+        print('none coin')
+        exit()
+
+    print(possessionCoin)
+
+    websocket_url = 'wss://pubwss.bithumb.com/pub/ws'
+    ws = websocket.WebSocketApp(websocket_url,
+                                on_open=on_open,
+                                on_message=on_message,
+                                on_close=on_close,
+                                on_error=on_error)
+
+    ws.run_forever()
+
+
+start()
